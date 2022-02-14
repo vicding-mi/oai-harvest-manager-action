50ffe0614a075b7550c40c26932bea4e0e9a67b8
Vic <qiqing.ding@di.huc.knaw.nl>
2022-02-14 18:55:54 +0100

    use huygens maven for dependency

--- src/main/java/nl/knaw/huc/di/sd/HelloWorldAction.java
+++ src/main/java/nl/knaw/huc/di/sd/HelloWorldAction.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class HelloWorldAction implements Action {
     @Override
     public boolean perform(List<Metadata> list) {
+//        TODO: check metadata format, what it is now, what can it be
+        // is it xml only now? metadata should be abstract/interface
+        // support oai and json
         System.out.println("Hello World! Performing!");
         return false;
     }
